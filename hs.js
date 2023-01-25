12083bde6db2112d18e50f43bddea35de204ce2d
tucker <teddog2013@gmail.com>
2023-01-25 15:30:19 +0700

    Finished bio

--- hs.js
+++ hs.js
@@ -1,17 +1,17 @@
 //BIO SLIDING AND FADE-IN TEXT//
 // Select the text element
-const text = document.querySelector('.bio-text');
+const bioText = document.querySelector('.bio-text');
 
 // Add the 'visible' class to the text element when the page loads
 window.onload = function() {
-    text.classList.add('visible');
+    bioText.classList.add('visible');
 };
 
 // Create a function to handle the sliding effect
-function slideText() {
-    text.style.transform = "translateX(0)";
+function slideBioText() {
+    bioText.style.transform = "translateX(0)";
 }
 
 // Call the slideText function after a delay of 2 seconds
-setTimeout(slideText, 700);
+setTimeout(slideBioText, 700);
 //END OF BIO SLIDING AND FADE-IN TEXT//
