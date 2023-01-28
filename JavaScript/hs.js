f5d47e992ffa2e25d00fc406afbdd944507299a5
tucker <teddog2013@gmail.com>
2023-01-28 16:48:09 +0700

    reverted changes

--- JavaScript/hs.js
+++ JavaScript/hs.js
@@ -15,14 +15,3 @@ function slideBioText() {
 // Call the slideText function after a delay of 2 seconds
 setTimeout(slideBioText, 700);
 //END OF BIO SLIDING AND FADE-IN TEXT//
-
-
-//REDUCE IMG SIZE//
-if ( window.innerWidth <= 767 ) {
-    var itTakesTwo = document.querySelector(".modpngs.itTakesTwo");
-    var noTime = document.querySelector(".modpngs.noTime");
-    var projectWinter = document.querySelector(".modpngs.projectWinter");
-    itTakesTwo.src = "pngs/130/itt-130.webp";
-    noTime.src = "pngs/130/nttrl-130.webp";
-    projectWinter.src = "pngs/130/pjwt-130.webp";
-}
